34ec7ce9b141dde24c7c7b6aaee9741136e647b1
Will Smith <wsmith@linode.com>
2017-06-08 07:00:14 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name="linode-cli",
-    version="1.0.2b",
+    version="1.0.3b",
     description="CLI for Linode API v4",
     long_description=long_description,
     author="Linode",
