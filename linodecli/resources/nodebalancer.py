d9aaa008ed3a3f7ccb0e42d5708add6126de2227
Will Smith <wsmith@linode.com>
2017-06-08 21:42:15 -0400

    Adds NodeBalancer config-create
    Ability to create NodeBalancer Configs

--- linodecli/resources/nodebalancer.py
+++ linodecli/resources/nodebalancer.py
@@ -104,3 +104,41 @@ throttle: {}"""
 
         n = _get_nodebalancer_or_die(client, args.label)
         n.delete()
+
+    def config_create(args, client, unparsed=None):
+        parser = argparse.ArgumentParser(description="Create a NodeBalancer Config (port)")
+        parser.add_argument('label', metavar='LABEL', type=str,
+                help="The NodeBalancer to add the Config to.")
+        parser.add_argument('-p', '--port', metavar='PORT', type=int, default=80,
+                help="The NodeBalancer config port to bind on (1-65534).  Default is 80")
+        parser.add_argument('-L', '--protocol', metavar='PROTOCOL', type=str, default='http',
+                help="One of 'tcp', 'http', and 'https'")
+        parser.add_argument('-A', '--algorithm', metavar='ALGORITHM', type=str, default='roundrobin',
+                help="Balancing algorithm.  Options are 'roundrobin', 'leastconn', and 'source'")
+        parser.add_argument('-S', '--stickiness', metavar='STICKINESS', type=str, default='table',
+                help="Session persistence.  One of 'none', 'table', 'http_cookie'")
+        parser.add_argument('-H', '--checkhealth', metavar='CHECKHEALTH', type=str, default='connection',
+                help="Perform acive health checks on the backend nodes.  One of 'connection', 'http', or 'http_body'")
+        parser.add_argument('-I', '--checkinterval', metavar="CHECKINTERVAL", type=int, default=5,
+                help="Seconds between health check probes (2-3600)")
+        parser.add_argument('-T', '--checktimeout', metavar='CHECKTIMEOUT', type=int, default=3,
+                help="Seconds to wait before considering the probe a failure (1-30)")
+        parser.add_argument('-X', '--checkattempts', metavar='CHECKATTEMPTS', type=int, default=2,
+                help="Number of failed probes before taking a node out of rotation (1-30)")
+        parser.add_argument('-P', '--checkpath', metavar='CHECKPATH', type=str, default='/',
+                help="When check-health='http', the path to request.")
+        parser.add_argument('-B', '--checkbody', metavar="CHECKBODY", type=str,
+                help="When check-health='http_body', a regex against the expected result body.")
+        parser.add_argument('-C', '--sslcert', metavar="SSLCERT", type=str,
+                help="SSL certificate served by the NodeBalancer when the protocol is 'https'")
+        parser.add_argument('-K', '--sslkey', metavar="SSLKEY", type=str,
+                help="Unpassphrased private key for teh SSL certificate whe protocol is 'https'")
+
+        args = parser.parse_args(args=unparsed, namespace=args)
+
+        n = _get_nodebalancer_or_die(client, args.label)
+
+        n.create_config(port=args.port, label="port {}".format(args.port), protocol=args.protocol, algorithm=args.algorithm,
+                stickiness=args.stickiness, check=args.checkhealth, check_interval=args.checkinterval, check_health=args.checkhealth,
+                check_timeout=args.checktimeout, check_attempts=args.checkattempts, check_path=args.checkpath, check_body=args.checkbody,
+                ssl_cert=args.sslcert, ssl_key=args.sslkey)
