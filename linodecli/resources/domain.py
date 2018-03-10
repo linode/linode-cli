27171e863dbe96991b1e280a9c07a4bab4071a56
Stefan Liebenberg <siga.fredo@gmail.com>
2018-03-10 22:22:19 +0100

    addign missing str2bool

--- linodecli/resources/domain.py
+++ linodecli/resources/domain.py
@@ -358,6 +358,13 @@ master ips: {}
         for record_to_delete in to_delete:
            record_to_delete.delete()
 
+    def str2bool(v):
+        if v.lower() in ('yes', 'true', 't', 'y', '1'):
+           return True
+        elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+           return False
+        else:
+           raise argparse.ArgumentTypeError('Boolean value expected.')
 
 
     def record_show(args, client, unparsed=None):
