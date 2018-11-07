22aaa8120125a22ab425eb4675247d897da321e3
Will Smith <wsmith@linode.com>
2018-11-07 16:59:17 -0500

    Added Markdown output format
    Closes #67
    
    As requested, prints out output in a github-styled markdown format

--- linodecli/__init__.py
+++ linodecli/__init__.py
@@ -42,6 +42,8 @@ def main():
                         help="The delimiter when displaying raw output.")
     parser.add_argument('--json', action='store_true',
                         help="Display output as JSON")
+    parser.add_argument('--markdown', action='store_true',
+                        help="Display output in Markdown format.")
     parser.add_argument('--pretty', action='store_true',
                         help="If set, pretty-print JSON output")
     parser.add_argument('--no-headers', action='store_true',
@@ -71,6 +73,8 @@ def main():
         cli.output_handler.mode = OutputMode.delimited
     elif parsed.json:
         cli.output_handler.mode = OutputMode.json
+    elif parsed.markdown:
+        cli.output_handler.mode = OutputMode.markdown
     if parsed.delimiter:
         cli.output_handler.delimiter = parsed.delimiter
     if parsed.pretty:
