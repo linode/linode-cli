b131bd8408e0e2b1d39ef4c9071124a191197e38
Patrick Thiel <pthiel@linode.com>
2019-09-26 14:17:45 -0400

    Run tests as root, for now

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,14 +8,12 @@ docker build \
   --build-arg SPEC="${SPEC}" \
   -t linode-cli-test .
 
-docker run -e TOKEN_1="${TOKEN_1}" \
+docker run \
+    -e TOKEN_1="${TOKEN_1}" \
     -e TOKEN_2="${TOKEN_2}" \
     -e TEST_ENVIRONMENT="${ENVIRONMENT}" \
     -e DOCKER_BATS="TRUE" \
     -e RUN_LONG_TESTS="${LONG_TESTS}" \
-    -e USER=${USER} \
-    -e USERID=$(grep Uid /proc/self/status | cut -f2 | awk '{$1=$1};1') \
-    -u $(id -u) \
     -v $SSH_PRIVATE_KEY:/src/.ssh/id_rsa \
     -v $SSH_PUBLIC_KEY:/src/.ssh/id_rsa.pub \
     --rm \
