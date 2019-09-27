3643834e1587bcafd5c269f1fcf255dfef46628e
Patrick Thiel <pthiel@linode.com>
2019-09-27 13:51:07 -0400

    execute publish as jenkins user

--- scripts/cipublish.sh
+++ scripts/cipublish.sh
@@ -3,6 +3,9 @@
 docker run \
     --entrypoint "" \
     --rm \
+    -e USER=${USER} \
+    -e USERID=$(grep Uid /proc/self/status | cut -f2 | awk '{$1=$1};1') \
+    -u $(id -u) \
     -v $(pwd):/src \
     linode-cli \
     twine upload -u "${pypi_user}" -p "${pypi_password}" "/src/dist/*.whl"
