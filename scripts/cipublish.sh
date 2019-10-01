b1986e69373158ae602153eb8cf2ee962cb4758a
Patrick Thiel <pthiel@linode.com>
2019-10-01 14:56:19 -0400

    fix docker image reference

--- scripts/cipublish.sh
+++ scripts/cipublish.sh
@@ -9,5 +9,5 @@ docker run \
     -e USERID=$(grep Uid /proc/self/status | cut -f2 | awk '{$1=$1};1') \
     -u $(id -u) \
     -v $(pwd):/src \
-    linode-cli \
+    linode-cli-$BUILD_TAG \
     twine upload -u "${pypi_user}" -p "${pypi_password}" "/src/dist/*.whl"
