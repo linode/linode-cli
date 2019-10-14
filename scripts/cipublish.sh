0ea919f9176532c86d5e8cf89c3d1cb9d12c818c
Patrick Thiel <pthiel@linode.com>
2019-10-14 15:29:56 -0400

    shorten image tags

--- scripts/cipublish.sh
+++ scripts/cipublish.sh
@@ -9,5 +9,5 @@ docker run \
     -e USERID=$(grep Uid /proc/self/status | cut -f2 | awk '{$1=$1};1') \
     -u $(id -u) \
     -v $(pwd):/src \
-    "linode-cli-$BUILD_TAG:latest" \
+    "cli-builder:${BUILD_TAG}" \
     twine upload -u "${pypi_user}" -p "${pypi_password}" "/src/dist/*.whl"
