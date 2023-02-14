6fc0465b220cb56d6cbd03d080fc7ed5b35fd62a
Zhiwei Liang <zliang@akamai.com>
2023-02-14 14:16:29 -0500

    Format everything

--- linodecli/plugins/obj.py
+++ linodecli/plugins/obj.py
@@ -19,9 +19,9 @@ from typing import List
 from terminaltables import SingleTable
 
 from linodecli.cli import CLI
-from linodecli.plugins import PluginContext
 from linodecli.configuration import _do_get_request
 from linodecli.configuration.helpers import _default_thing_input
+from linodecli.plugins import PluginContext
 
 ENV_ACCESS_KEY_NAME = "LINODE_CLI_OBJ_ACCESS_KEY"
 ENV_SECRET_KEY_NAME = "LINODE_CLI_OBJ_SECRET_KEY"
@@ -975,7 +975,7 @@ def _get_boto_client(cluster, access_key, secret_key):
     return client
 
 
-def _get_s3_creds(client: CLI, force: bool=False):
+def _get_s3_creds(client: CLI, force: bool = False):
     """
     Retrieves stored s3 creds for the acting user from the config, or generates new
     creds using the client and stores them if none exist
@@ -1076,22 +1076,18 @@ def _configure_plugin(client: CLI):
     clusters = [
         c["id"]
         for c in _do_get_request(  # pylint: disable=protected-access
-            client.config.base_url, 
+            client.config.base_url,
             "/object-storage/clusters",
             token=client.config.get_value("token"),
-        )[
-            "data"
-        ]
+        )["data"]
     ]
 
-    cluster = (
-        _default_thing_input(  # pylint: disable=protected-access
-            "Configure a default Cluster for operations.",
-            clusters,
-            "Default Cluster: ",
-            "Please select a valid Cluster",
-            optional=False,  # this is the only configuration right now
-        )
+    cluster = _default_thing_input(  # pylint: disable=protected-access
+        "Configure a default Cluster for operations.",
+        clusters,
+        "Default Cluster: ",
+        "Please select a valid Cluster",
+        optional=False,  # this is the only configuration right now
     )
 
     client.config.plugin_set_value("cluster", cluster)
