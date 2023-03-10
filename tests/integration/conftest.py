4b8c2357c3aeda358a86cdf8e928aaad499554bf
Zhiwei Liang <121905282+zliang-akamai@users.noreply.github.com>
2023-03-10 14:04:55 -0500

    Put obj Plugin Integration Test Files in a Tempfile Directory (#395)
    ## 📝 Description
    
    Python `tempfile` module helps to clean up the generated files.
    Preventing junk files left when test cases fail.
    
    ## ✔️ How to Test
    
    `make testint`

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -6,9 +6,12 @@ import subprocess
 import tempfile
 from collections import defaultdict
 from itertools import count
+from pathlib import Path
 from random import randint
+from typing import Callable, Optional
 
 import pytest
+from helpers import get_random_text
 
 from linodecli import ENV_TOKEN_NAME
 
@@ -59,3 +62,29 @@ def token():
             f"Token is required in the environment as {ENV_TOKEN_NAME}"
         )
     return token
+
+
+@pytest.fixture
+def generate_test_files(name_generator: Callable[[str], str]):
+    """
+    Return a function that can generate files with random text.
+    """
+    test_files_dir = tempfile.TemporaryDirectory()
+
+    def _generate_test_files(
+        num: Optional[int] = 1, content: Optional[str] = None
+    ):
+        if content is None:
+            content = f"Linode CLI integration test\n{get_random_text(100)}\n"
+        file_paths = [
+            Path(test_files_dir.name) / f"{name_generator('test-file')}.txt"
+            for _ in range(num)
+        ]
+        file_paths = [f.resolve() for f in file_paths]
+        for f in file_paths:
+            with open(f, "w") as f:
+                f.write(content)
+        return file_paths
+
+    yield _generate_test_files
+    test_files_dir.cleanup()
