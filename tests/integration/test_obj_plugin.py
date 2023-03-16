0c3ecb528e60e563e479ce464b63b0a646e0e83c
ykim-1 <ykim@akamai.com>
2023-03-16 13:07:09 -0700

    make format

--- tests/integration/test_obj_plugin.py
+++ tests/integration/test_obj_plugin.py
@@ -13,6 +13,8 @@ from pytest import MonkeyPatch
 
 from linodecli.configuration.auth import _do_request
 from linodecli.plugins.obj import ENV_ACCESS_KEY_NAME, ENV_SECRET_KEY_NAME
+from tests.integration.fixture_types import GetTestFileType
+from tests.integration.helpers import BASE_URL
 
 REGION = "us-southeast-1"
 BASE_CMD = ["linode-cli", "obj", "--cluster", REGION]
