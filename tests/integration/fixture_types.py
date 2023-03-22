81c82b0093afc1fe6e16fcbdfe66c9da244587a8
Zhiwei Liang <121905282+zliang-akamai@users.noreply.github.com>
2023-03-22 18:17:39 -0400

    Object Storage Website Hosting Integration Test (#404)
    ## 📝 Description
    
    More tests, more reliability.
    
    ## ✔️ How to Test
    
    `export LINODE_CLI_TOKEN=<TOKEN>`
    `make testint`

--- tests/integration/fixture_types.py
+++ tests/integration/fixture_types.py
@@ -4,4 +4,5 @@ Complicated type alias for fixtures and other stuff.
 from pathlib import Path
 from typing import Callable, List, Optional
 
-GetTestFileType = Callable[[Optional[int], Optional[str]], List[Path]]
+GetTestFilesType = Callable[[Optional[int], Optional[str]], List[Path]]
+GetTestFileType = Callable[[Optional[str], Optional[str]], Path]
