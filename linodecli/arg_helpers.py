8874fb292b6c71ce7c66a0662a039aa013f6d344
noprobelm <noprobelm@protonmail.com>
2023-04-18 12:00:30 -0400

    Corrected plugin_content to plugin_proc

--- linodecli/arg_helpers.py
+++ linodecli/arg_helpers.py
@@ -284,7 +284,7 @@ def help_with_ops(ops, config):
             plugin_proc.append(plugin_content[i + 3 :])
 
         plugin_table = Table(show_header=False)
-        for plugin in plugin_content:
+        for plugin in plugin_proc:
             plugin_table.add_row(*plugin)
         rprint(plugin_table)
 
