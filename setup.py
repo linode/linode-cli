8373c165364916961e9c3124d1ebc6da3fa463c7
ykim-1 <ykim@akamai.com>
2023-05-24 13:51:16 -0700

    Changed windows case in get_baked_file()

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def get_baked_files():
             data_files.append(("/etc/bash_completion.d", ["linode-cli.sh"]))
     else:
         if path.isfile("linode-cli.sh"):
-            data_files.append(["linode-cli.sh"])
+            data_files.append(("C:\Program Files\Git\mingw64\share\git\completion", ["linode-cli.sh"]))
 
     return data_files
 
