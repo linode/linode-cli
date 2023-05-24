6c040feb3e5adc145c83ff9fa9ac8316b2b8314b
ykim-1 <ykim@akamai.com>
2023-05-24 11:07:21 -0700

    Path fix attempt 7 pulling my hair out

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def get_baked_files():
     """
     data_files = []
 
-    if here.is_file("linode-cli.sh"):
+    if path.is_file("linode-cli.sh"):
         data_files.append(("/etc/bash_completion.d", ["linode-cli.sh"]))
 
     return data_files
