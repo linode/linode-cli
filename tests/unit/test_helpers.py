36e75c0762aced563167871b691831ab3e35681e
Youjung Kim <126618609+ykim-1@users.noreply.github.com>
2023-08-11 10:10:05 -0600

    Add unit tests for shared args functions (#16)

--- tests/unit/test_helpers.py
+++ tests/unit/test_helpers.py
@@ -1,4 +1,10 @@
-from linodecli.helpers import filter_markdown_links
+from argparse import ArgumentParser
+
+from linodecli.helpers import (
+    filter_markdown_links,
+    pagination_args_shared,
+    register_args_shared,
+)
 
 
 class TestHelpers:
@@ -14,3 +20,25 @@ class TestHelpers:
         )
 
         assert filter_markdown_links(original_text) == expected_text
+
+    def test_pagination_args_shared(self):
+        parser = ArgumentParser()
+        pagination_args_shared(parser)
+
+        args = parser.parse_args(
+            ["--page", "2", "--page-size", "50", "--all-rows"]
+        )
+        assert args.page == 2
+        assert args.page_size == 50
+        assert args.all_rows
+
+    def test_register_args_shared(self):
+        parser = ArgumentParser()
+        register_args_shared(parser)
+
+        args = parser.parse_args(
+            ["--as-user", "linode-user", "--suppress-warnings"]
+        )
+        assert args.as_user == "linode-user"
+        assert args.suppress_warnings
+
