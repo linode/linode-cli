21810785f96ff658ddf1494891d113d0d585149c
ykim-1 <ykim@akamai.com>
2023-09-11 11:25:45 -0700

    add version to xml

--- scripts/add_to_xml_test_report.py
+++ scripts/add_to_xml_test_report.py
@@ -3,44 +3,25 @@ import xml.etree.ElementTree as ET
 
 # Parse command-line arguments
 parser = argparse.ArgumentParser(description='Modify XML with workflow information')
-parser.add_argument('--workflow-id', required=True)
-parser.add_argument('--workflow-run-number', required=True)
-parser.add_argument('--repository', required=True)
-parser.add_argument('--head-branch', required=True)
-parser.add_argument('--commit-sha', required=True)
+parser.add_argument('--semanticversion', required=True)
+parser.add_argument('--xmlfile', required=True)  # Added argument for XML file path
 
 args = parser.parse_args()
 
 # Open and parse the XML file
-xml_file_path = 'your_xml_report.xml'  # Replace with your XML file path
+xml_file_path = args.xmlfile
 tree = ET.parse(xml_file_path)
 root = tree.getroot()
 
 # Create new elements for the information
-workflow_id_element = ET.Element('workflow_id')
-workflow_id_element.text = args.workflow_id
-
-workflow_run_number_element = ET.Element('workflow_run_number')
-workflow_run_number_element.text = args.workflow_run_number
-
-repository_element = ET.Element('repository')
-repository_element.text = args.repository
-
-head_branch_element = ET.Element('head_branch')
-head_branch_element.text = args.head_branch
-
-commit_sha_element = ET.Element('commit_sha')
-commit_sha_element.text = args.commit_sha
+semanticversion_element = ET.Element('semanticversion')
+semanticversion_element.text = args.semanticversion
 
 # Add the new elements to the root of the XML
-root.append(workflow_id_element)
-root.append(workflow_run_number_element)
-root.append(repository_element)
-root.append(head_branch_element)
-root.append(commit_sha_element)
+root.append(semanticversion_element)
 
 # Save the modified XML
-modified_xml_file_path = 'modified_xml_report.xml'  # Replace with the desired output path
+modified_xml_file_path = xml_file_path  # Overwrite it
 tree.write(modified_xml_file_path)
 
 print(f'Modified XML saved to {modified_xml_file_path}')
