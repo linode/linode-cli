499bc1650f221763df788588a76ae6e278fd8660
ykim-1 <ykim@akamai.com>
2023-09-11 12:49:46 -0700

    adding more fields

--- scripts/add_to_xml_test_report.py
+++ scripts/add_to_xml_test_report.py
@@ -3,7 +3,9 @@ import xml.etree.ElementTree as ET
 
 # Parse command-line arguments
 parser = argparse.ArgumentParser(description='Modify XML with workflow information')
-parser.add_argument('--semanticversion', required=True)
+parser.add_argument('--branch_name', required=True)
+parser.add_argument('--gha_run_id', required=True)
+parser.add_argument('--gha_run_number', required=True)
 parser.add_argument('--xmlfile', required=True)  # Added argument for XML file path
 
 args = parser.parse_args()
@@ -14,11 +16,19 @@ tree = ET.parse(xml_file_path)
 root = tree.getroot()
 
 # Create new elements for the information
-semanticversion_element = ET.Element('semanticversion')
-semanticversion_element.text = args.semanticversion
+branch_name_element = ET.Element('branch_name')
+branch_name_element.text = args.branch_name
+
+gha_run_id_element = ET.Element('gha_run_id')
+gha_run_id_element.text = args.gha_run_id
+
+gha_run_number_element = ET.Element('gha_run_number')
+gha_run_number_element.text = args.gha_run_number
 
 # Add the new elements to the root of the XML
-root.append(semanticversion_element)
+root.append(branch_name_element)
+root.append(gha_run_id_element)
+root.append(gha_run_number_element)
 
 # Save the modified XML
 modified_xml_file_path = xml_file_path  # Overwrite it
