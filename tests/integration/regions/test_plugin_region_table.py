c4b5469266972b8c69b2fcb70f281e410d5cdb64
Jacob Riddle <87780794+jriddle-linode@users.noreply.github.com>
2023-10-06 15:12:38 -0400

    new: adding `region-table` plugin (#525)
    ## 📝 Description
    
    **What does this PR do and why is this change necessary?**
    
    Plugin adding `region-table` command to the CLI.
    ```bash
    lin region-table
    ```
    
    ## ✔️ How to Test
    
    **How do I run the relevant unit/integration tests?**
    
    `make INTEGRATION_TEST_PATH=regions testint`
    
    resolves #492

---
+++ tests/integration/regions/test_plugin_region_table.py
@@ -0,0 +1,23 @@
+import subprocess
+from typing import List
+
+BASE_CMD = ["linode-cli", "region-table"]
+
+
+def exec_test_command(args: List[str]):
+    process = subprocess.run(
+        args,
+        stdout=subprocess.PIPE,
+    )
+    return process
+
+
+def test_output():
+    process = exec_test_command(BASE_CMD)
+    output = process.stdout.decode()
+    lines = output.split("\n")
+    lines = lines[3 : len(lines) - 2]
+    for line in lines:
+        assert "-" in line
+        assert "✔" in line
+        assert "|" in line
