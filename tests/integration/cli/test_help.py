22f977d583332b7f0666e47259a83c8130e7c7c3
ykim-1 <ykim@akamai.com>
2024-01-15 12:37:05 -0800

    fix assertions

--- tests/integration/cli/test_help.py
+++ tests/integration/cli/test_help.py
@@ -1,5 +1,5 @@
 import pytest
-
+import textwrap
 from tests.integration.helpers import exec_test_command
 
 
@@ -7,25 +7,27 @@ from tests.integration.helpers import exec_test_command
 def test_help_page_for_non_aliased_actions():
     process = exec_test_command(["linode-cli", "linodes", "list", "--help"])
     output = process.stdout.decode()
+    wrapped_output = textwrap.fill(output, width=150).replace("\n", "")
 
-    assert "Linodes List" in output
+    assert "Linodes List" in wrapped_output
     assert (
-        "API Documentation: https://www.linode.com/docs/api/linode-instances/#linodes"
-        in output
+        "API Documentation:  https://www.linode.com/docs/api/linode-instances/#linodes-list"
+        in wrapped_output
     )
-    assert "You may filter results with:" in output
-    assert "--tags" in output
+    assert "You may filter results with:" in wrapped_output
+    assert "--tags" in wrapped_output
 
 
 @pytest.mark.smoke
 def test_help_page_for_aliased_actions():
     process = exec_test_command(["linode-cli", "linodes", "ls", "--help"])
     output = process.stdout.decode()
+    wrapped_output = textwrap.fill(output, width=150).replace("\n", "")
 
-    assert "Linodes List" in output
+    assert "Linodes List" in wrapped_output
     assert (
-        "API Documentation: https://www.linode.com/docs/api/linode-instances/#linodes"
-        in output
+        "API Documentation:  https://www.linode.com/docs/api/linode-instances/#linodes-list"
+        in wrapped_output
     )
-    assert "You may filter results with:" in output
-    assert "--tags" in output
+    assert "You may filter results with:" in wrapped_output
+    assert "--tags" in wrapped_output
