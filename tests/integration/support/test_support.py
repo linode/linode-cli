39f3160ae135bb20cd89f279eec3a1cbee5398d7
vshanthe <vshanthe@akamai.com>
2024-04-30 20:13:47 +0530

    integration test

---
+++ tests/integration/support/test_support.py
@@ -0,0 +1,101 @@
+import pytest
+
+from tests.integration.helpers import assert_headers_in_lines, exec_test_command
+from tests.integration.linodes.test_linodes import setup_linodes
+
+BASE_CMD = ["linode-cli", "tickets"]
+
+
+# this will create a support ticket on your account
+def test_create_support_ticket(setup_linodes):
+    linode_id = setup_linodes
+    exec_test_command(
+        BASE_CMD
+        + [
+            "create",
+            "--description",
+            "Creating support ticket for test verification",
+            "--linode_id",
+            linode_id,
+            "--summary",
+            "Testing ticket" "--text",
+            "--no-headers",
+        ]
+    )
+
+
+def test_tickets_list():
+    res = (
+        exec_test_command(BASE_CMD + ["list", "--text", "--delimiter=,"])
+        .stdout.decode()
+        .rstrip()
+    )
+    lines = res.splitlines()
+    headers = ["summary", "opened_by", "opened"]
+    assert_headers_in_lines(headers, lines)
+
+
+@pytest.fixture
+def get_ticket_id():
+    ticket_ids = (
+        exec_test_command(
+            BASE_CMD
+            + [
+                "list",
+                "--text",
+                "--no-headers",
+                "--delimiter",
+                ",",
+                "--format",
+                "id",
+            ]
+        )
+        .stdout.decode()
+        .rstrip()
+        .split(",")
+    )
+    first_id = ticket_ids[0]
+    yield first_id
+
+
+def test_tickets_view(get_ticket_id):
+    ticket_id = get_ticket_id
+    res = (
+        exec_test_command(
+            BASE_CMD + ["view", ticket_id, "--text", "--delimiter=,"]
+        )
+        .stdout.decode()
+        .rstrip()
+    )
+    lines = res.splitlines()
+    headers = ["summary", "opened_by", "opened"]
+    assert_headers_in_lines(headers, lines)
+
+
+def test_reply_support_ticket(get_ticket_id):
+    ticket_id = get_ticket_id
+    exec_test_command(
+        BASE_CMD
+        + [
+            "reply",
+            ticket_id,
+            "--description",
+            "test reply on the support ticket",
+            "--text",
+            "--no-headers",
+        ]
+    )
+
+
+def test_view_replies_support_ticket(get_ticket_id):
+    ticket_id = get_ticket_id
+    res = (
+        exec_test_command(
+            BASE_CMD + ["replies", ticket_id, "--text", "--delimiter=,"]
+        )
+        .stdout.decode()
+        .rstrip()
+    )
+    lines = res.splitlines()
+    headers = ["created_by", "created"]
+    assert_headers_in_lines(headers, lines)
---
+++ tests/integration/users/test_profile.py
@@ -0,0 +1,118 @@
+import pytest
+
+from tests.integration.helpers import assert_headers_in_lines, exec_test_command
+
+BASE_CMD = ["linode-cli", "profile"]
+
+
+def test_profile_view():
+    res = (
+        exec_test_command(BASE_CMD + ["view", "--text", "--delimiter=,"])
+        .stdout.decode()
+        .rstrip()
+    )
+    lines = res.splitlines()
+    headers = ["username", "email", "restricted"]
+    assert_headers_in_lines(headers, lines)
+
+
+def test_profile_apps_list():
+    res = (
+        exec_test_command(BASE_CMD + ["apps-list", "--text", "--delimiter=,"])
+        .stdout.decode()
+        .rstrip()
+    )
+    lines = res.splitlines()
+    headers = ["label", "scopes", "website"]
+    assert_headers_in_lines(headers, lines)
+
+
+def test_profile_devices_list():
+    res = (
+        exec_test_command(
+            BASE_CMD + ["devices-list", "--text", "--delimiter=,"]
+        )
+        .stdout.decode()
+        .rstrip()
+    )
+    lines = res.splitlines()
+    headers = ["created", "expiry", "user_agent"]
+    assert_headers_in_lines(headers, lines)
+
+
+@pytest.fixture
+def get_login_id():
+    login_id = (
+        exec_test_command(
+            BASE_CMD
+            + [
+                "logins-list",
+                "--text",
+                "--no-headers",
+                "--delimiter",
+                ",",
+                "--format",
+                "id",
+            ]
+        )
+        .stdout.decode()
+        .rstrip()
+        .splitlines()
+    )
+    first_login_id = login_id[0]
+    yield first_login_id
+
+
+def test_profile_login_list():
+    res = (
+        exec_test_command(BASE_CMD + ["logins-list", "--text", "--delimiter=,"])
+        .stdout.decode()
+        .rstrip()
+    )
+    lines = res.splitlines()
+    headers = ["datetime", "username", "status"]
+    assert_headers_in_lines(headers, lines)
+
+
+def test_profile_login_view(get_login_id):
+    login_id = get_login_id
+    res = (
+        exec_test_command(
+            BASE_CMD + ["login-view", login_id, "--text", "--delimiter=,"]
+        )
+        .stdout.decode()
+        .rstrip()
+    )
+    lines = res.splitlines()
+    headers = ["datetime", "username", "status"]
+    assert_headers_in_lines(headers, lines)
+
+
+def test_security_questions_list():
+    res = (
+        exec_test_command(
+            [
+                "linode-cli",
+                "security-questions",
+                "list",
+                "--text",
+                "--delimiter=,",
+            ]
+        )
+        .stdout.decode()
+        .rstrip()
+    )
+    lines = res.splitlines()
+    headers = ["security_questions.id", "security_questions.question"]
+    assert_headers_in_lines(headers, lines)
+
+
+def test_profile_token_list():
+    res = (
+        exec_test_command(BASE_CMD + ["tokens-list", "--text", "--delimiter=,"])
+        .stdout.decode()
+        .rstrip()
+    )
+    lines = res.splitlines()
+    headers = ["label", "scopes", "token"]
+    assert_headers_in_lines(headers, lines)
