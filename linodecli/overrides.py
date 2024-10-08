601dc5469d7bff57ed707b29b3a2f9166316fbe6
Ye Chen <127243817+yec-akamai@users.noreply.github.com>
2024-10-08 16:26:13 -0400

    Override images replicate output (#653)

--- linodecli/overrides.py
+++ linodecli/overrides.py
@@ -4,7 +4,7 @@ This allows us to easily alter per-command outputs, etc. without making
 large changes to the OpenAPI spec.
 """
 
-from typing import Dict
+from typing import Dict, List
 
 from rich import box
 from rich import print as rprint
@@ -55,6 +55,15 @@ def handle_types_region_prices_list(
     Override the output of 'linode-cli linodes types' to display regional pricing.
     """
     return linode_types_with_region_prices(operation, output_handler, json_data)
+
+
+@output_override("images", "replicate", OutputMode.table)
+def handle_image_replicate(operation, output_handler, json_data) -> bool:
+    # pylint: disable=unused-argument
+    """
+    Override the output of 'linode-cli images replicate'.
+    """
+    return image_replicate_output(json_data)
 
 
 def linode_types_with_region_prices(
@@ -137,3 +146,45 @@ def format_region_prices(data: Dict[str, any]) -> any:
         sub_table.add_row(*region_price_row)
 
     return sub_table
+
+
+def build_replicas_output(replicas: List) -> Table:
+    """
+    Format nested replicas list to a sub-table.
+    """
+    replicas_output = Table(show_header=False, box=None)
+    replicas_headers = replicas[0].keys()
+    for replica in replicas:
+        row = []
+        for h in replicas_headers:
+            row.append(Align(str(replica[h]), align="left"))
+        replicas_output.add_row(*row)
+
+    return replicas_output
+
+
+def image_replicate_output(json_data) -> bool:
+    """
+    Parse and format the image replicate output table.
+    """
+    output = Table(
+        header_style="bold",
+        show_lines=True,
+    )
+
+    row = []
+    for header in json_data.keys():
+        if header == "regions" and len(json_data[header]) > 0:
+            # leverage `replicas` in output for readability
+            output.add_column("replicas", justify="center")
+            row.append(build_replicas_output(json_data[header]))
+        elif json_data[header] is not None:
+            output.add_column(header, justify="center")
+            row.append(Align(str(json_data[header]), align="left"))
+
+    output.add_row(*row)
+
+    console = Console()
+    console.print(output)
+
+    return False
