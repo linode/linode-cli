afdc3b223bb0d483f9992937aa8c9d71e2bf3880
Youjung Kim <126618609+ykim-akamai@users.noreply.github.com>
2024-10-30 09:16:15 -0700

    CI: Notify slack channel on releases; Add test coverage for vlans and lke types (#665)
    Co-authored-by: Youjung Kim <126618609+ykim-1@users.noreply.github.com>

---
+++ tests/integration/vlans/test_vlans.py
@@ -0,0 +1,62 @@
+from tests.integration.helpers import assert_headers_in_lines, exec_test_command
+
+BASE_CMD = ["linode-cli", "vlans"]
+
+
+def test_list_vlans():
+    types = (
+        exec_test_command(
+            BASE_CMD
+            + [
+                "ls",
+                "--text",
+            ]
+        )
+        .stdout.decode()
+        .rstrip()
+    )
+
+    headers = ["region", "label", "linodes"]
+    lines = types.splitlines()
+
+    assert_headers_in_lines(headers, lines)
+
+
+def test_list_vlans_help_menu():
+    help_menu = (
+        exec_test_command(
+            BASE_CMD
+            + [
+                "ls",
+                "--h",
+            ]
+        )
+        .stdout.decode()
+        .rstrip()
+    )
+
+    assert "linode-cli vlans ls\nList VLANs\n" in help_menu
+    assert (
+        "https://techdocs.akamai.com/linode-api/reference/get-vlans"
+        in help_menu
+    )
+
+
+def test_delete_vlans_help_menu():
+    help_menu = (
+        exec_test_command(
+            BASE_CMD
+            + [
+                "delete",
+                "--h",
+            ]
+        )
+        .stdout.decode()
+        .rstrip()
+    )
+
+    assert "linode-cli vlans delete [LABEL] [REGIONID]" in help_menu
+    assert (
+        "https://techdocs.akamai.com/linode-api/reference/delete-vlan"
+        in help_menu
+    )
