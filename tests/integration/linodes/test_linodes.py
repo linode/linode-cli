a517ddd41f1c154bad04d1b7ab3c3952d1601818
ykim-1 <ykim@akamai.com>
2024-11-08 11:24:43 -0800

    update encryption tests

--- tests/integration/linodes/test_linodes.py
+++ tests/integration/linodes/test_linodes.py
@@ -256,7 +256,13 @@ def test_create_linode_disk_encryption_enabled(linode_cloud_firewall):
     res = (
         exec_test_command(
             BASE_CMD
-            + ["view", linode_id, "--text", "--delimiter=,", "--format=id,disk_encryption"]
+            + [
+                "view",
+                linode_id,
+                "--text",
+                "--delimiter=,",
+                "--format=id,disk_encryption",
+            ]
         )
         .stdout.decode()
         .rstrip()
@@ -284,7 +290,13 @@ def test_create_linode_disk_encryption_disabled(linode_cloud_firewall):
     res = (
         exec_test_command(
             BASE_CMD
-            + ["view", linode_id, "--text", "--delimiter=,", "--format=id,disk_encryption"]
+            + [
+                "view",
+                linode_id,
+                "--text",
+                "--delimiter=,",
+                "--format=id,disk_encryption",
+            ]
         )
         .stdout.decode()
         .rstrip()
