00810aa9fd17cbdf186cc726c3ed45a274c763af
Grzegorz Krasoń <grzegorz.krason@gmail.com>
2025-03-05 15:45:46 +0100

    Update linodecli/help_formatter.py
    Co-authored-by: Zhiwei Liang <121905282+zliang-akamai@users.noreply.github.com>

--- linodecli/help_formatter.py
+++ linodecli/help_formatter.py
@@ -6,6 +6,10 @@ from argparse import HelpFormatter
 from operator import attrgetter
 
 class SortingHelpFormatter(HelpFormatter):
+    """
+    The formatter class for help menu.
+    """
+
     def add_arguments(self, actions):
         actions = sorted(actions, key=attrgetter('option_strings'))
         super(SortingHelpFormatter, self).add_arguments(actions)
