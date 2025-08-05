b03c1b0703d374cc6e31c8c14a3b14b2198b87d0
Pawel Snoch <psnoch@akamai.com>
2025-08-05 11:28:07 +0200

    Add integration tests for node balancer with UDP configuration

--- tests/integration/nodebalancers/fixtures.py
+++ tests/integration/nodebalancers/fixtures.py
@@ -165,3 +165,161 @@ def nodebalancer_with_default_conf(linode_cloud_firewall):
     res_arr = result.split(",")
     nodebalancer_id = res_arr[0]
     delete_target_id(target="nodebalancers", id=nodebalancer_id)
+
+
+@pytest.fixture(scope="module")
+def nodebalancer_with_udp_config_and_node(linode_cloud_firewall):
+    nodebalancer_id = exec_test_command(
+        BASE_CMDS["nodebalancers"]
+        + [
+            "create",
+            "--region",
+            "us-ord",
+            "--client_conn_throttle",
+            "20",
+            "--firewall_id",
+            linode_cloud_firewall,
+            "--text",
+            "--delimiter",
+            ",",
+            "--format",
+            "id",
+            "--no-headers",
+        ]
+    )
+    config_id = exec_test_command(
+        BASE_CMDS["nodebalancers"]
+        + [
+            "config-create",
+            nodebalancer_id,
+            "--port",
+            "80",
+            "--protocol",
+            "udp",
+            "--algorithm",
+            "roundrobin",
+            "--check_interval",
+            "90",
+            "--check_timeout",
+            "10",
+            "--check_attempts",
+            "3",
+            "--check_path",
+            "/test",
+            "--check_body",
+            "it works",
+            "--delimiter",
+            ",",
+            "--text",
+            "--no-headers",
+            "--format",
+            "id",
+        ]
+    )
+
+    linode_create = exec_test_command(
+        BASE_CMDS["linodes"]
+        + [
+            "create",
+            "--root_pass",
+            "aComplex@Password",
+            "--booted",
+            "true",
+            "--region",
+            "us-ord",
+            "--type",
+            "g6-nanode-1",
+            "--private_ip",
+            "true",
+            "--image",
+            DEFAULT_TEST_IMAGE,
+            "--firewall_id",
+            linode_cloud_firewall,
+            "--text",
+            "--delimiter",
+            ",",
+            "--format",
+            "id,ipv4",
+            "--no-header",
+            "--suppress-warnings",
+        ]
+    )
+    linode_arr = linode_create.split(",")
+    linode_id = linode_arr[0]
+    ip_arr = linode_arr[1].split(" ")
+    node_ip = ip_arr[1]
+    node_label = "defaultnode1"
+    node_id = exec_test_command(
+        BASE_CMDS["nodebalancers"]
+        + [
+            "node-create",
+            nodebalancer_id,
+            config_id,
+            "--address",
+            node_ip + ":80",
+            "--label",
+            node_label,
+            "--weight",
+            "100",
+            "--delimiter",
+            ",",
+            "--text",
+            "--no-headers",
+            "--format",
+            "id",
+        ]
+    )
+
+    yield nodebalancer_id, config_id, node_id, node_ip
+
+    delete_target_id(target="nodebalancers", id=nodebalancer_id)
+    delete_target_id(target="linodes", id=linode_id)
+
+
+@pytest.fixture(scope="module")
+def simple_nodebalancer_with_config(linode_cloud_firewall):
+    nodebalancer_id = exec_test_command(
+        BASE_CMDS["nodebalancers"]
+        + [
+            "create",
+            "--region",
+            "us-ord",
+            "--client_conn_throttle",
+            "20",
+            "--firewall_id",
+            linode_cloud_firewall,
+            "--text",
+            "--delimiter",
+            ",",
+            "--format",
+            "id",
+            "--no-headers",
+        ]
+    )
+    config_id = exec_test_command(
+        BASE_CMDS["nodebalancers"]
+        + [
+            "config-create",
+            nodebalancer_id,
+            "--port",
+            "81",
+            "--protocol",
+            "http",
+            "--algorithm",
+            "leastconn",
+            "--check_path",
+            "/test",
+            "--check_body",
+            "it works",
+            "--text",
+            "--no-headers",
+            "--delimiter",
+            ",",
+            "--format",
+            "id",
+        ]
+    )
+
+    yield nodebalancer_id, config_id
+
+    delete_target_id(target="nodebalancers", id=nodebalancer_id)
